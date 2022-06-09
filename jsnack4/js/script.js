bdb124cc4df59500f3a2ee7a017f6e7ea6a65943
NicoloYan <nico.yan@outlook.it>
2022-06-09 16:33:24 +0200

    Some details have been corrected

---
+++ jsnack4/js/script.js
@@ -0,0 +1,23 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+// Creo un array vuoto
+let numbers = []
+
+let userNumber
+
+// Genero i prompt per l'utente per ottenere i numeri
+    // se il numero è dispari, popolo l'array   
+
+for ( let i = 0; i < 6; i++ ) {
+    userNumber = parseInt(prompt('Dammi un numero'))
+    if (userNumber % 2 != 0) {
+        numbers.push(userNumber)
+    } else {
+
+    }
+}
+
+// Stampo in console l'array con gli eventuali numeri dispari
+console.log(numbers)
